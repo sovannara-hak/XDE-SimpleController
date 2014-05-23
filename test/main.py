97cc4a39e34084364e4896088aee7eb54fb8dc47
shak <sovannara.hak@isir.upmc.fr>
2014-05-23 16:40:50 +0200

    Fix test/main.py for XDE-1124

--- test/main.py
+++ test/main.py
@@ -2,8 +2,8 @@ import xde_world_manager as xwm
 import xde_resources as xr
 import xde_robot_loader as xrl
 
-import dsimi.interactive
-shell = dsimi.interactive.shell()
+import xdefw.interactive
+shell = xdefw.interactive.shell()
 
 import deploy.deployer as ddeployer
 
@@ -19,20 +19,25 @@ wm = xwm.WorldManager()
 wm.createAllAgents(TIME_STEP)
 
 
-import xde_spacemouse as spacemouse
-
 sphereWorld = xrl.createWorldFromUrdfFile(xr.sphere, "sphere", [0,0.6,1.2, 1, 0, 0, 0], False, 0.2, 0.005)# , "material.concrete")
 kukaWorld = xrl.createWorldFromUrdfFile(xr.kuka, "k1g", [0,0,0.4, 1, 0, 0, 0], True, 1, 0.005) #, "material.concrete")
 
 wm.addMarkers(sphereWorld, ["sphere.sphere"], thin_markers=False)
-wm.addWorld(sphereWorld, True)
-wm.addWorld(kukaWorld, True)
+wm.addWorld(sphereWorld)
+wm.addWorld(kukaWorld)
 
 #Controller
-control = dsimi.rtt.Task(ddeployer.load("control", "XDE_SimpleController", "XDESimpleController-gnulinux", "", libdir="/home/shak/src/xde/XDE-SimpleController/_build/src/"))
+control = xdefw.rtt.Task(ddeployer.load("control", "XDE_SimpleController", "XDESimpleController-gnulinux", "", libdir="/tmp/XDE-SimpleController/_build/src/"))
 
-model = physicshelper.createDynamicModel(kukaWorld, "k1g")
-control.s.setDynModel(str(model.this.__long__()))
+import xde.desc.physic.physic_pb2
+model = xde.desc.physic.physic_pb2.MultiBodyModel()
+model.kinematic_tree.CopyFrom(kukaWorld.scene.physical_scene.nodes[0])
+model.meshes.extend(kukaWorld.library.meshes)
+model.mechanism.CopyFrom(kukaWorld.scene.physical_scene.mechanisms[0])
+model.composites.extend(kukaWorld.scene.physical_scene.collision_scene.meshes)
+dynmodel = physicshelper.createDynamicModel(model)
+
+control.s.setDynModel(str(dynmodel.this.__long__()))
 
 #create connectors to get robot k1g state 'k1g_q', 'k1g_qdot', 'k1g_Hroot', 'k1g_Troot', 'k1g_H'
 robot_name = "k1g"
@@ -56,12 +61,4 @@ kuka.setJointVelocities(lgsm.vector([0.0]*7))
 control.s.setPeriod(TIME_STEP)
 control.s.start()
 
-#PDC Control mode
-#sm = spacemouse.createTask("smi", TIME_STEP, wm.phy, wm.graph, "sphere.sphere", pdc_enabled=True, body_name="k1g.07")
-
-# Normal mode
-#sm = spacemouse.createTask("smi", TIME_STEP, wm.phy, wm.graph, "sphere.sphere", pdc_enabled=False)
-
-#sm.s.start()
-
-wm.startSimulation()
+wm.startAgents()
